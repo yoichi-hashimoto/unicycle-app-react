cc657baa66dea4669b5167561973b689c3a8706d
hakaiou <hakaiou20067@gmail.com>
2026-09-17 10:23:13 +0900

    fix xsrf configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =
-  process.env.REACT_APP_API_URL || "http://localhost:8000";
-axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+
+  withCredentials: true,
+
+  withXSRFToken: true,
+
+  xsrfCookieName: "XSRF-TOKEN",
+
+  xsrfHeaderName: "X-XSRF-TOKEN",
+});
 export default axios;
