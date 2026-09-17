273a18213714dead9a5538ecd0d06926a9a4585b
hakaiou <hakaiou20067@gmail.com>
2026-09-17 09:32:33 +0900

    configure production API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL || "http://localhost:8000";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 export default axios;
